ca7254e9271935aebce6d459f36f4131d8f61383
Sean Downs <spdowns@protonmail.com>
2020-06-25 10:01:52 -0700

    finish 1st test with a pass. writes 2nd test to split at period
    Co-authored-by: Erich Richter <erich.richter@gmail.com>

--- __tests__/haiku.tests.js
+++ __tests__/haiku.tests.js
@@ -1,9 +1,13 @@
 import { Haiku } from "./../src/haiku.js"
 
 describe('Haiku', () => {
-  
-  test("should correctly split the text at return cariage and/or period",() => {
-    const haiku = new Haiku("This is a test string. This should be lines. And one last word, smores");
-    expect(haiku.line1).toEqual("This is a test string. This should be lines. And one last word, smores");
-    });
+
+  test("should correctly split the text at return cariage and/or period", () => {
+    const haiku = new Haiku("This is a test string. This should be↵lines. And one ↵last word, smores");
+    expect(haiku.line1).toEqual("This is a test string. This should be↵lines. And one ↵last word, smores");
   });
+
+  test("should correctly split the text at a peroid", () => {
+const
+  });
+});
--- src/haiku.js
+++ src/haiku.js
@@ -1,27 +1,25 @@
 export class Haiku {
-    constructor(line1){
-        this.line1 = line1;
-    }  
-    
-    // lineSplit() {
-    //   return line1.split(/\r\n/);
-    
-    //  }
-    
-    lines() {
-      return this.line1.split(/\r\n/);
-      //console.log(lines())
-    }
-    
-    lineCount() {
-      return this.lines().length -1;
-      
-    }
-    
-    //console.log(lineCount());
+  constructor(line1) {
+    this.line1 = line1;
+  }
 
-    
-    }
+  // lineSplit() {
+  //   return line1.split('.');
+  //   }
+
+  // lines() {
+  //   return this.line1.split(/\r\n/);
+  //   //console.log(lines())
+  // }
+
+  // lineCount() {
+  //   return this.lines().length -1;
+
+  // }
+
+  //console.log(lineCount());
+
+}
 
 // String.prototype.lines = function() { return this.split(/\r*\n/); }
 // String.prototype.lineCount = function() { return this.lines().length -1; 
