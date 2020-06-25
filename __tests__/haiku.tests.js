d4c300078656f497f6cf63b857fd1a94679eba82
Sean Downs <spdowns@protonmail.com>
2020-06-25 11:39:34 -0700

    add proper syntax on calling a prototype method in es6 in test
    Co-authored-by: Erich Richter <erich.richter@gmail.com>

--- __tests__/haiku.tests.js
+++ __tests__/haiku.tests.js
@@ -2,15 +2,16 @@ import { Haiku } from "./../src/haiku.js"
 
 describe('Haiku', () => {
 
-  test("should correctly split the text at return cariage and/or period", () => {
+  test("should correctly declare a haiku", () => {
     const haiku = new Haiku("This is a test string. This should be lines. And one last word, smores");
     expect(haiku.line1).toEqual("This is a test string. This should be lines. And one last word, smores");
   });
 
   test("should correctly split the text at a peroid", () => {
   const haiku = new Haiku("This is a test string. This should be lines. And one last word, smores.",[]);
+  haiku.lineSplit()
   console.log(haiku)
-  expect(haiku.line1Arr).toEqual(["This is a test string", "This should be lines", "And one last word, smores."])
+  expect(haiku.line1Arr).any("This is a test string", " This should be lines", " And one last word, smores.")
   });
 });
 
--- src/haiku.js
+++ src/haiku.js
@@ -1,14 +1,14 @@
 export class Haiku {
   constructor(line1, line1Arr) {
     this.line1 = line1;
-    this.line1Arr = line1Arr;
+    this.line1Arr = [];
   }
 
   lineSplit() {
     let newArray = this.line1.split('.');
     this.line1Arr.push(newArray);
     }
-  
+
     
   // lines() {
   //   return this.line1.split(/\r\n/);
@@ -23,6 +23,7 @@ export class Haiku {
   //console.log(lineCount());
 
 }
+
 
 // String.prototype.lines = function() { return this.split(/\r*\n/); }
 // String.prototype.lineCount = function() { return this.lines().length -1; 
